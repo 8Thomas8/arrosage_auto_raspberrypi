1c366dd1c56ef63ee1773255bbd48ce79074b54f
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-03-01 17:40:45 +0100

    Programme de base, correction sur les fonctions crées précédemment, et création des dernières fonctions. Corrections et refacto par rapport à la source.

--- app.py
+++ app.py
@@ -2,32 +2,34 @@ import RPi.GPIO as GPIO
 import datetime
 import time
 
-init = False
-
-# Status du dernier arrosage
-lw_status_code = 1
+# Source: http://www.cyber-omelette.com/2017/09/automated-plant-watering.html
+# Avec refacto et customisation 
 
 # Selectionne le type de schema des GPIO
 GPIO.setmode(GPIO.BOARD)
+
+# Definition des variables
+water_sensor_pin = 8
+pump_pin = 7
+time_pump_on = 3
+delay_before_auto_watering = 5
 
 
 # Verifie le dernier arrosage
 def get_last_watered():
     try:
-        global lw_status_code
         # Ouvre le fichier en lecture
         f = open("last_watered.txt", "r")
-        lw_status_code = 0
         return f.readline()
-    except len(lw_status_code) != 0:
+    except IOError:
         return "JAMAIS"
 
 
 # Recupere le status du pin definit
-def get_status(pin=8):
+def get_status():
     # Set la direction du pin
-    GPIO.setup(pin, GPIO.IN)
-    return GPIO.input(pin)
+    GPIO.setup(water_sensor_pin, GPIO.IN)
+    return GPIO.input(water_sensor_pin)
 
 
 # Initialise le status du pin definit
@@ -38,6 +40,38 @@ def init_output(pin):
     GPIO.output(pin, GPIO.LOW)
     GPIO.output(pin, GPIO.HIGH)
 
-# Function auto_water(delay = 5, pump_pin = 5, water_sensor_pin = 8)
 
-# Function pump_on(pump_pin = 7, delay = 1)
+# Gere l'arrosage automatique
+def auto_water():
+    consecutive_water_count = 0
+    init_output(pump_pin)
+    print("Arrosage automatique lancé, faites CTRL+C pour quitter.")
+    try:
+        while True:
+            time.sleep(delay_before_auto_watering)
+            wet = get_status()
+            print("Humide (0 = oui, 1 = non)= " + str(wet))
+            if wet != 0 and consecutive_water_count <= 5:
+                pump_on()
+                consecutive_water_count += 1
+            elif wet != 0 and consecutive_water_count > 5:
+                time.sleep(20)
+                consecutive_water_count = 0
+            else:
+                consecutive_water_count = 0
+    except KeyboardInterrupt:
+        print("Arrosage automatique interrompu.")
+        GPIO.cleanup()
+
+
+# Active la pompe
+def pump_on():
+    init_output(pump_pin)
+    # Ouvre le fichier en mode écriture
+    f = open("last_watered.txt", "w")
+    f.write("Dernier arrosage {}".format(datetime.datetime.now()))
+    f.close()
+    # Active puis désactive la pompe
+    GPIO.output(pump_pin, GPIO.LOW)
+    time.sleep(time_pump_on)
+    GPIO.output(pump_pin, GPIO.HIGH)
